84ddd657d0a266d1f9d6954be8f06992a58270dc
ritah@LAPTOP-O8O1L7T5 <haidar_rita@etna-alternance.net>
2023-05-11 14:37:53 +0200

    update

--- exo.js
+++ exo.js
@@ -1,1 +1,19 @@
-yfyjfyuyu
+
+//méthode pour crée un générateur de mots de passe complexes
+function generatePassword(length) {
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    return password;
+  }
+
+  // génère un mot de passe de 12 caractères
+  console.log(generatePassword(12)); 
+
+ 
+  
+
+  
